67a00c26f3d1a094657536ece4e156227f6935cb
Alex Chan <Alex.Y.Chan.19@dartmouth.edu>
2017-02-10 15:18:48 -0500

    category model and controller

---
+++ app/controllers/categoryController.js
@@ -0,0 +1,47 @@
+import CategoryModel from '../models/categoryModel';
+
+export const createCategory = (req, res) => {
+  const category = new CategoryModel();
+  category.name = req.body.name;
+  category.url = req.body.url;
+  category.gradeLevel = req.body.gradeLevel;
+  category.img = req.body.img;
+  category.save()
+    .then(result => {
+      res.json({ message: 'category created!', category });
+    })
+    .catch(error => {
+      res.json({ error });
+    });
+};
+
+// TODO: build this function
+export const getcategory = (req, res) => {
+  CategoryModel.findById(req.params.id)
+    .then(result => {
+      res.json({ message: 'Single Category found!', category: result });
+    })
+    .catch(error => {
+      res.json({ error });
+    });
+};
+
+export const deleteCategory = (req, res) => {
+  CategoryModel.remove({ _id: req.params.id })
+    .then(result => {
+      res.json({ message: 'Category Removed!', category: result });
+    })
+    .catch(error => {
+      res.json({ error });
+    });
+};
+
+export const updateCategory = (req, res) => {
+  CategoryModel.update({ _id: req.params.id }, req.body)
+    .then(result => {
+      res.json({ message: 'Category Updated!', category: result });
+    })
+    .catch(error => {
+      res.json({ error });
+    });
+};
---
+++ app/models/categoryModel.js
@@ -0,0 +1,20 @@
+// categoryModel.js
+
+import mongoose, { Schema } from 'mongoose';
+
+// the schema for the category model
+const categorySchema = new Schema({
+  categoryName: {
+    type: String,
+    required: true,
+  }, // 1=>autism, 2=>hearing, 3=>visual, 4=>learning, 5=>physical, 6=>social, 7=>speech, 8=>cognitive
+  gradeLevel: Number,
+  url: String,  // url where the html for the category can be found
+  img: String, // url to the image
+});
+
+// create model class
+const categoryModel = mongoose.model('Category', categorySchema);
+
+// export model class to use in other files
+export default categoryModel;
